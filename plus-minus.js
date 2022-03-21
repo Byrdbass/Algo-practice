ac3dd5fce1d94981ca8492f3db49773ebad81fa6
ByrdBassPC <byrdbass@gmail.com>
2022-03-21 15:45:50 -0500

    setup plus-minus algo from hackerrank

---
+++ plus-minus.js
@@ -0,0 +1,6 @@
+//Given an array of integers, calculate the ratios of it's elements that are positive, negative, and zero.  
+//Print the decimal value of each fraction on a new line with 6 places after the decimal
+
+const array1 = [1,1,0,-1,-1]
+//there are two positive numbers, two negative numbers, and one zero
+//there are five numbers total
