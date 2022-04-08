0cf0d69da39d0952e5aff4f1b988d912873ced0a
ByrdBassPC <byrdbass@gmail.com>
2022-04-08 17:23:48 -0500

    made attempt at if statement trying to get test case to work.  Having trouble returning expected result when all values are the same in the array

--- mini-maxSum.js
+++ mini-maxSum.js
@@ -1,27 +1,42 @@
 //Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
 //Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
-arr = [256741038, 623958417, 467905213, 714532089, 938071625]
+arr = [6, 5, 5, 5, 5]
+let newArr = [...arr]
+
+var isSame  = true;
+function compare(newArr) {
+    
+    for (let i=0; i<newArr.length; i++) {
+        isSame = newArr[0] === newArr[i] ? true : false;
+    }
+    console.log(isSame);
+}
+
 // find the minimum value of an array and store to a variable
-const minValue = Math.min(...arr)
-//console.log(minValue)
+const minValue = Math.min(newArr)
+console.log(minValue)
+newArr.filter((v, i) => newArr.indexOf(v) != i)
+newArr.splice(minValue, 1)
+console.log(newArr)
 // loop through the array and compare it to the min value, if it is greater than the min value push these indexes into a new array
-const maxArray = arr.filter(i => i>minValue)
+const maxArray = newArr.filter(i => i>=minValue)
+console.log(maxArray)
 // add up all the elements of the max array
-const maxSum = maxArray.reduce((total, curValue) => total +curValue, 0);
+const maxSum = maxArray.reduce((total, curValue) => total + curValue, 0);
 console.log(maxSum)
 
 
 
 // find the maximum value of the array and store to a variable
-const maxValue = Math.max(...arr)
+const maxValue = Math.max(...newArr)
 //console.log(maxValue)
 // loop through the array and compare it to the max value, if it is less than the max value push these indexes into a new array called minArray
 const minArray = arr.filter(i => i<maxValue)
 //console.log(minArray)
 // add up all the elements of the minArray
 const minSum = minArray.reduce((total, curValue) => total + curValue, 0)
-console.log(minSum)
+// console.log(minSum)
 
 // print with two spaces in between each returned value in one line
 console.log(minSum + '  ' + maxSum)
