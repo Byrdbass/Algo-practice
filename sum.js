23cb5056bce14430f76e591894157cc7d074201c
ByrdBassPC <byrdbass@gmail.com>
2022-04-14 13:31:04 -0500

    made adjustments to sum3 using reduce method and correctly logged time complexity attempts with peformance.now() method

--- sum.js
+++ sum.js
@@ -3,6 +3,7 @@ const arr = [1,2,3,4,5]
 function sum () {
     //spreadArr = [...arr]
     let result = 0
+    //could use for (const number of numbers) { result += number };
     for (i=0;i<=arr.length;i++) {
         result += i
     }
@@ -14,9 +15,9 @@ sum()
 let end1 = performance.now()
 console.log(end1-start1)
 
+// CONSTANT TIME COMPLEXITY?
 function sum2 () {
     let result =0
-    // let spreadArr = [...arr]
     arr.forEach(x => result += x)
     return console.log(result)
 }
@@ -24,4 +25,15 @@ function sum2 () {
 let start2 = performance.now()
 sum2()
 let end2 = performance.now()
-console.log(end2-end1)
+console.log(end2-start2)
+
+function sum3 () {
+    let result = 0
+    arr.reduce( (reduceSum, currNum) => reduceSum + currNum, 0)
+    return console.log(result)
+}
+
+let start3 = performance.now()
+sum3()
+let end3 = performance.now()
+console.log(end3-start3)
