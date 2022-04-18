3bd589407736720d93558a49725c7d908b518a56
byrdbassGit <byrdbass@gmail.com>
2022-04-18 13:32:38 -0500

    found solution by .sort method and comparing indexes by looping through and seeing if index was not equal to next value as well as previous value

--- hackerrankAlgos/lonely-integer.js
+++ hackerrankAlgos/lonely-integer.js
@@ -3,6 +3,8 @@
 // loop through the array and find if elements match the value of other elements
 // use an if statement to compare 
 
+let array = [4,5,6,7,8,7,6,5,4]
+
 //3 WAYS TO GET UNIQUE VALUES - VIA Codú Community YouTube
 const getUniqueValues = (array) => (
     array.filter((currentValue, index, arr) => (
@@ -10,29 +12,43 @@ const getUniqueValues = (array) => (
     ))
 )
 
+console.log('filter and index of ',getUniqueValues(array))
+
 const getUniqueValues2 = (array) => (
     array.reduce((accumulatedValue, currentValue) => (
         accumulatedValue.includes(currentValue) ? accumulatedValue 
         : [...accumulatedValue, currentValue]
-    ))
+    ), [])
 )
+console.log('reduce and includes ',getUniqueValues2(array))
 
-const getUniqueValues3 = (array) => {
+const getUniqueValues3 = (array) => (
     [... new Set(array)]
-}
+)
+console.log('spread new Set ',getUniqueValues3(array))
 
-function lonelyinteger() {
-    let a = [1,2,3,4,3,2,1]
-    // use spread operator to make copy of the values in the array
-    function isUnique(value, index, arr) {
-        return arr.indexOf(value) === index;
+function lonelyinteger(a) {
+    //let len = a.length
+    a.sort(function(a,b) {return a-b});
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== a[i+1] && a[i] !== a[i-1]) {
+            console.log(a[i])
+            return a[i]
+        }
     }
-    let unique = a.filter(isUnique)
-    let arr = a.splice(unique)
-    console.log(unique)
-    console.log(arr)
+    // use spread operator to make copy of the values in the array
+    // function isUnique(value, index, arr) {
+    //     return arr.indexOf(value) === index;
+    // }
+    // let unique = a.filter(isUnique)
+    // let arr = a.splice(unique)
+    // console.log(unique)
+    // console.log(arr)
 
 }
+
+const a = [1,2,3,4,3,2,1]
+
     //console.log(arr.forEach())
     // loop through the array and compare the values to original array
     // for (i=0;i<=a.length;i++)
@@ -59,4 +75,4 @@ function lonelyinteger() {
 // }
 //isEqual();
 
-lonelyinteger();
+console.log(lonelyinteger(a))
