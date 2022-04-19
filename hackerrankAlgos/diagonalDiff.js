4c7d95d63adaffd2c043003691ef079ad20b3e1f
byrdbassGit <byrdbass@gmail.com>
2022-04-19 08:22:29 -0500

    started logic on diagonalDiff algo, by using nested for loops and converting to a string using split method.  Attempting to use slice for pulling correct values out of matrix

---
+++ hackerrankAlgos/diagonalDiff.js
@@ -0,0 +1,21 @@
+//  Given a square matrix, calculate the difference between the sums of its diagonals
+
+const diagonalDiff = (arr) => {
+    let len = arr.length
+    let str = ''
+    for (let i=0;i<len;i++) {
+        let subArray = arr[i];
+        for (let j=0;j<len;j++) {
+            //console.log(subArray[j])
+            //console.log(str += subArray[j])
+            str += subArray[j]
+            
+        }
+    }
+    //console.log(str.split(''))
+    let strSplit = str.split('')
+    console.log(strSplit.slice(0, 4))
+}
+const arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+diagonalDiff(arr);
