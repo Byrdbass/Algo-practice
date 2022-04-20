07abe3395c1f7cf14a0f36afc20b67c3f160fc74
byrdbassGit <byrdbass@gmail.com>
2022-04-20 11:11:02 -0500

    found a way to multiply all values together, but the initial for loop is only passing in the first index aka the first array to the reduce function to find the product

--- method-list.js
+++ method-list.js
@@ -4,7 +4,10 @@ arr.find()
 arr.findIndex()
 // removes an index in the array arr.splice(index to remove, remove this many elements from here on) i.e. arr.splice(2, 1) = remove index 2, and one element
 arr.splice()
-arr.reduce()
+
+arr.reduce(function(previousValue, currentValue, currentIndex, array) {previousValue += currentValue}, initialValue)
+    arr.reduce((total, currentValue, currentIndex, array) => {total += currentValue}, initialValue)
+    
 arr.split()
 arr.includes()
 arr.indexOf()
