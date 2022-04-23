b8a0875c0826c0d830ed623a5b83818fb1ec0744
byrdbassGit <byrdbass@gmail.com>
2022-04-23 15:35:07 -0500

    solved sum of diaganols only when it is a matrix of 3x3, and was not able to solve once adding another value to matrix

--- hackerrankAlgos/diagonalDiff.js
+++ hackerrankAlgos/diagonalDiff.js
@@ -7,11 +7,16 @@ const diagonalDiff = (arr) => {
     //let str = ''
     for (let i=0;i<len;i++) {
         console.log(arr[i])
-        
+        let diag1Index = 0
+        let diag2Index = len-1
         let subArray = arr[i];
-        diag1.push(subArray[0])
+        console.log(subArray)
+        diag1.push(subArray[diag1Index+i])
+        diag2.push(subArray[diag2Index-i])
+  
+        
         //diag1.push(subArray[len+2])
-        for (let j=0;j<len;j++) {
+       // for (let j=0;j<len;j++) {
             
             //diag1.push(j+=3)
             
@@ -19,15 +24,21 @@ const diagonalDiff = (arr) => {
             //console.log(str += subArray[j])
             //str += subArray[j]
             
-        }
+        //}
     }
+    diag1result = 0
+    diag2result = 0
+    diag1.forEach(x => diag1result += x)
+    diag2.forEach(y => diag2result += y)
+
     //console.log(str.split(''))
     //let strSplit = str.split('')
     // console.log(strSplit.slice(len-len, 1))
     // console.log(strSplit).slice(len+2,1)
-    console.log(diag1)
-    return diag1
+    console.log(diag1, diag2)
+    console.log(diag1result, diag2result)
+    return 
 }
-const arr = [[1,2,3],[4,5,6],[7,8,9]]
+const arr = [[1,2,3,10],[4,5,6,10],[7,8,9,10]]
 
 diagonalDiff(arr);
