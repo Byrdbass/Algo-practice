fef9c18e266f0d8fcc79c0501addc7ccd47cc6d0
ByrdBassPC <byrdbass@gmail.com>
2022-04-27 15:12:44 -0500

    started working the counting sort algo by finding the range of values in the original array and attempting to push those to a new array

--- CLASSalgos/03-Javascript/test/arithmetic.test.js
+++ CLASSalgos/03-Javascript/test/arithmetic.test.js
@@ -1,31 +1,33 @@
 const assert = require('assert');
+const { expect } = require('chai')
 const { add, subtract, multiply, divide } = require('../arithmetic.js')
 
 describe('the add function', () => {
     it('should add 2 numbers together', () => {
         const result = add(5,5)
-        assert.equal(result, 10); 
+        //assert.equal(result, 10); 
+        
     }); 
 });
 
 describe('the subtract function', () => {
     it('should find the absolute difference between the numbers', () =>{
         const result = subtract(5,10)
-        assert.equal(result, 5);
+        //assert.equal(result, 5);
     });
 });
 
 describe('the multiply function', () => {
     it('should find the product of the two numbers', () =>{
         const result = multiply(5,5)
-        assert.equal(result, 25);
+        //assert.equal(result, 25);
     });
 });
 
 describe('the divide function', () => {
     it('should find the quotient between the numbers', () =>{
         const result = divide(25, 5)
-        assert.equal(result, 5);
+        //assert.equal(result, 5);
     });
 });
 
--- sortingAlgos/countingSort.js
+++ sortingAlgos/countingSort.js
@@ -1,1 +1,27 @@
 // given a list of integers, count and return the number of times each value appears as an array of integers
+
+//this works great when the range of values is small
+
+const countingSort = (arr) => {
+    //need to establish the range of values in the array
+    let len = arr.length
+    let minVal = Math.min(...arr)
+    let maxVal = Math.max(...arr)
+    let range = (maxVal - minVal) + 1
+    let emptyArr = []
+    let sortedArr = emptyArr.push(0*range)
+    console.log(minVal, maxVal, range,sortedArr)
+    for (i=0;i<len;i++) {
+
+    }
+
+    
+
+
+
+    return sortedArr;
+}
+
+array = [1,1,1,4,4,5,5]
+
+countingSort(array);
