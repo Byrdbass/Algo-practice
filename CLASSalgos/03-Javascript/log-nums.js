39d1d215ea03a510b858b17b17f2d3dc14b0d951
ByrdBassPC <byrdbass@gmail.com>
2022-05-03 14:05:12 -0500

    solved log-nums.js algo, need to create a unit test

--- CLASSalgos/03-Javascript/log-nums.js
+++ CLASSalgos/03-Javascript/log-nums.js
@@ -1,9 +1,16 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    let start = 1
+    for (let i = 0; i < num-1; i ++){
+        
+        console.log(start ++);
+    }
+    return start
+};
 
 num1 = 10;
-logNums(num1);
+console.log(logNums(num1));
 
 module.exports = { logNums };
--- method-list.js
+++ method-list.js
@@ -1,23 +1,32 @@
 // ARRAY METHODS that take in a function
 const arr = [1,2,3,4,5]
 
-arr.pop() //remove last element returns new index
-arr.push() // inserts new value at end
+arr.pop() //remove last element returns new index 0(1)
+arr.push() // inserts new value at end 0(1)
 
 arr.shift() //removes first element O(n)
-arr.unshift // inserts new value at beginning
+arr.unshift // inserts new value at beginning O(n)
 
-arr.concat()
-arr.slice()
-arr.sort()
+arr.concat() // adds to arrays together O(n)
+arr.slice() // returns copy of entire or part of an array O(n)
+
+// removes an index in the array arr.splice(index to remove, remove this many elements from here on) i.e. arr.splice(2, 1) = remove index 2, and one element
+arr.splice() // remove and add new elements - very versatile can be used to replace elements as well O(n)
+arr.sort() // is O(n log(n))!
+
+
+//ALL OF THESE ARE O(n)
+arr.forEach()
+arr.map()
+arr.filter()
+arr.reduce(function(previousValue, currentValue, currentIndex, array) {previousValue += currentValue}, initialValue)
+    arr.reduce((total, currentValue, currentIndex, array) => {total += currentValue}, initialValue)
 
 arr.find()
 arr.findIndex()
-// removes an index in the array arr.splice(index to remove, remove this many elements from here on) i.e. arr.splice(2, 1) = remove index 2, and one element
-arr.splice()
 
-arr.reduce(function(previousValue, currentValue, currentIndex, array) {previousValue += currentValue}, initialValue)
-    arr.reduce((total, currentValue, currentIndex, array) => {total += currentValue}, initialValue)
+
+
     
 arr.split()
 arr.includes()
