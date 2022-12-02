c353b8648ea904287c16aee25f496052a7ca6f25
ByrdBassPC <byrdbass@gmail.com>
2022-12-02 11:43:52 -0600

    deleted .git inside of 001-bigOexamples and started on AddOrMultiplyRuntimes class for Java algo study

---
+++ CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/AddOrMultiplyRuntimes.java
@@ -0,0 +1,22 @@
+package BigOExamples;
+
+class AddTheRuntimes {
+	Integer[] arrA = {2,4,6,8};
+	Integer[] arrB = {3,6,9,12,15};
+	
+	Integer[] addRuntime(Integer[] a, Integer[] b) {
+		return Integer[] C = 
+				for (Integer i : arrA) {
+					System.out.println(a);
+				}
+	}
+}
+
+public class AddOrMultiplyRuntimes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/DropTheConstants.java
+++ CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/DropTheConstants.java
@@ -1,15 +1,16 @@
 package BigOExamples;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class DropTheConstants {
 	//It is possible for 0(n) to run faster than O(1) in some cases!
 	//TWO non-nested for loops is not more precise
-	static int[] minAndMax(int[] arr) {
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		int[] minAndMaxArr = new int[2];
-		for(int x : arr) {
+	static Integer[] minAndMax(Integer[] arr) {
+		Integer min = Integer.MAX_VALUE;
+		Integer max = Integer.MIN_VALUE;
+		Integer[] minAndMaxArr = new Integer[2];
+		for(Integer x : arr) {
 			if(x<min) min = x;
 			if(x>max) max = x;
 		}
@@ -22,10 +23,10 @@ public class DropTheConstants {
 	}
 	
 	public static void main(String[] args) {
-		int[] array1 = {12,14,15,20,35,3};
-		List<int> Arrays.asList(minAndMax(array1));
+		Integer[] array1 = {12,14,15,20,35,3};
+		List<Integer> list1 = Arrays.asList(minAndMax(array1));
 		
-		System.out.println(Arrays.toString(minAndMax(array1)));
+		System.out.println(list1);
 
 	}
 
