3dd7ab4853408147dbaebaa9646f0519303e08aa
ByrdBassPC <byrdbass@gmail.com>
2022-12-05 14:46:20 -0600

    finished example of add and mulitply runtimes for Big O study

--- CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/AddOrMultiplyRuntimes.java
+++ CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/AddOrMultiplyRuntimes.java
@@ -1,21 +1,44 @@
 package BigOExamples;
 
-class AddTheRuntimes {
-	Integer[] arrA = {2,4,6,8};
-	Integer[] arrB = {3,6,9,12,15};
+import java.util.ArrayList;
+
+class Runtimes {
 	
-	Integer[] addRuntime(Integer[] a, Integer[] b) {
-		return Integer[] C = 
-				for (Integer i : arrA) {
-					System.out.println(a);
-				}
+	
+	//O(A+B)
+	public static void addRuntime(Integer[] a, Integer[] b) {
+		//two ways of writing an array - just for study purposes
+		ArrayList c = new ArrayList(a.length);
+		ArrayList<Integer> d = new ArrayList<Integer>(a.length);
+		
+		for (Integer i : a) {
+			System.out.println("index of a " + i);
+		}
+		for (Integer j : b) {
+			System.out.println("index of b " + j);
+		}
+				
+	}
+	
+	//O(A*B)
+	public static void mulitplyRuntime(Integer[] a, Integer[] b) {
+		for (Integer i: a) {
+			for (Integer j: b) {
+				System.out.println("multiply runtime method, a: " + i + " b: " + j);
+			}
+			
+		}
 	}
 }
 
 public class AddOrMultiplyRuntimes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer[] arrA = {2,4,6,8};
+		Integer[] arrB = {3,6,9,12,15};
+
+		Runtimes.addRuntime(arrA, arrB);
+		Runtimes.mulitplyRuntime(arrA, arrB);
 
 	}
 
