bb167f6511d0ad13f05cb3c7ff9f7f5ffb082ad3
ByrdBassPC <byrdbass@gmail.com>
2022-12-30 12:31:41 -0600

    created the recursive runtime algo for runtime of O(2^n)

--- CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/RecursiveRuntime.java
+++ CrackTheCodingInterview/001-BigOexamples/src/BigOExamples/RecursiveRuntime.java
@@ -1,6 +1,14 @@
 package BigOExamples;
 
 public class RecursiveRuntime {
+	
+	//this has runtime of O(2^N)
+	Integer f(Integer n) {
+		if (n<=1) {
+			return 1;
+		}
+		return f(n-1) + f(n-1);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
