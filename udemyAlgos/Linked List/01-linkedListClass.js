0d54c08e370df764ce698aefb5485d1d47eed6c0
Byrdbass <byrdbass@gmail.com>
2024-09-10 10:19:19 -0500

    added get method to linked list - made use of a place holder for current Node

--- udemyAlgos/Linked List/01-linkedListClass.js
+++ udemyAlgos/Linked List/01-linkedListClass.js
@@ -1,75 +1,87 @@
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  //add an element to the end of list
-  push(val) {
-    var newNode = new Node(val);
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      // the tail property exists on the "this" first, then each value is a Node and has the "next property"
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-
-    return this;
-  }
-  //remove an element from end of list
-  pop() {
-    if (!this.head) return undefined;
-    let temp = this.head; //or let current = this.head
-    let lastEl = temp; // or newTail = current
-    while (temp.next) {
-      console.log(temp.val);
-      lastEl = temp; //or newTail = current
-      temp = temp.next; // or current = current.next
-    }
-    this.tail = lastEl;
-    this.tail.next = null;
-    this.length--;
-    if( this.length === 0){
+    constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
-    console.log(`${lastEl.val} removed`);
-    
-    return lastEl;
-  }
-  //remove an element from the beginning
-  shift(){
-    if (!this.head) return undefined //or length is 0
-    let temp = this.head;
-    this.head = temp.next;
-    this.length--;
-    if(!this.head) this.tail = null
-    return temp;
-  }
-  //add an element to the beginning
-  unShift(val){
-    let newNode = new Node(val)
-    if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
+    //add an element to the end of list
+    push(val) {
+        var newNode = new Node(val);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            // the tail property exists on the "this" first, then each value is a Node and has the "next property"
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+
+        return this;
     }
-    else {
-        newNode.next = this.head;
-        this.head = newNode;
+    //remove an element from end of list
+    pop() {
+        if (!this.head) return undefined;
+        let temp = this.head; //or let current = this.head
+        let lastEl = temp; // or newTail = current
+        while (temp.next) {
+            console.log(temp.val);
+            lastEl = temp; //or newTail = current
+            temp = temp.next; // or current = current.next
+        }
+        this.tail = lastEl;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        console.log(`${lastEl.val} removed`);
+
+        return lastEl;
     }
-    this.length++;
-    return this;
-  }
+    //remove an element from the beginning
+    shift() {
+        if (!this.head) return undefined //or length is 0
+        let temp = this.head;
+        this.head = temp.next;
+        this.length--;
+        if (!this.head) this.tail = null
+        return temp;
+    }
+    //add an element to the beginning
+    unShift(val) {
+        let newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index ){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
 }
 
 var list = new SinglyLinkedList();
