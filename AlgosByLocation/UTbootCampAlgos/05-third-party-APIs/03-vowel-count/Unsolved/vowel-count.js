a03e81db38f473ce271118c5c1ab5f4833fd986a
ByrdBassPC <byrdbass@gmail.com>
2025-01-20 19:49:27 -0600

    made additions to regex file, and solved unit 5 in UTBootcamp folder

--- AlgosByLocation/UTbootCampAlgos/05-third-party-APIs/03-vowel-count/Unsolved/vowel-count.js
+++ AlgosByLocation/UTbootCampAlgos/05-third-party-APIs/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,13 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    str = str.toLowerCase();
+    console.log(str)
+    let vowels = /[aeiou]/;
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        console.log(str[i])
+        if (vowels.test(str[i])) count++
+    };
+    return count;
+};
--- algoTypes/regex.js
+++ algoTypes/regex.js
@@ -1,4 +1,13 @@
-const s = 'hello' //THIS IS A STRING OBJECT
-new String(hello)
-const r = /hello/
-new Re
+// Write code to return the the number of vowels in `str`
+
+var vowelCount = function(str) {
+    str = str.toLowerCase();
+    console.log(str)
+    let vowels = /[aeiou]/;
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        console.log(str[i])
+        if (vowels.test(str[i])) count++
+    };
+    return count;
+};
