a3d703239a38a52d611c87601a5f59091a8fa58f
Byrdbass <byrdbass@gmail.com>
2025-02-15 10:29:09 -0600

    added doubly linked list

---
+++ AlgosByLocation/udemyAlgos/Doubly Linked List/01-setup.js
@@ -0,0 +1,18 @@
+class ListNode {
+    constructor(val){
+        this.val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DblLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        //check to see if the list is empty - no head, no tail - length = 0
+    }
+}
